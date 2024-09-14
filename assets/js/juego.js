66a1a92b59aa4582458c0062356b405edd9f0258
garfdev42 <cepedasan007@gmail.com>
2024-09-13 23:54:38 -0500

    Commit sept 13

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -9,6 +9,7 @@ let puntosJugador = 0,
 //Referencias del HTML
 const btnPedir = document.querySelector("#btnPedir");
 
+const divCartasJugador = document.querySelector("#jugador-cartas");
 const puntosHTML = document.querySelectorAll("small");
 
 //Esta funcion crea un nuevo deck o una nueva baraja
@@ -70,4 +71,18 @@ btnPedir.addEventListener("click", () => {
   const carta = pedirCarta();
   puntosJugador = puntosJugador + valorCarta(carta);
   puntosHTML[0].innerText = puntosJugador;
+
+  const imgCarta = document.createElement("img");
+  imgCarta.src = `assets/cartas/${carta}.png`;
+  imgCarta.classList.add("carta");
+
+  divCartasJugador.append(imgCarta);
+
+  if (puntosJugador > 21) {
+    console.warn("Lo siento mucho, perdiste");
+    btnPedir.disabled = true;
+  } else if (puntosJugador === 21) {
+    console.warn("21, genial!");
+    btnPedir.disabled = true;
+  }
 });
