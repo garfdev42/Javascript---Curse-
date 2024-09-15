b522f5d5a67a096d169bbc294a5857e42bc9bc21
garfdev42 <cepedasan007@gmail.com>
2024-09-14 19:24:14 -0500

    Commit sep 14

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -8,8 +8,12 @@ let puntosJugador = 0,
 
 //Referencias del HTML
 const btnPedir = document.querySelector("#btnPedir");
+const btnDetener = document.querySelector("#btnDetener");
+const btnNuevo = document.querySelector("#btnNuevo");
 
 const divCartasJugador = document.querySelector("#jugador-cartas");
+const divCartasComputadora = document.querySelector("#computadora-cartas");
+
 const puntosHTML = document.querySelectorAll("small");
 
 //Esta funcion crea un nuevo deck o una nueva baraja
@@ -63,8 +67,37 @@ const valorCarta = (carta) => {
   // console.log(puntos);
 };
 
-const valor = valorCarta(pedirCarta());
-console.log({ valor });
+// const valor = valorCarta(pedirCarta());
+// console.log({ valor });
+
+const turnoComputadora = (puntosMinimos) => {
+  do {
+    const carta = pedirCarta();
+    puntosComputadora = puntosComputadora + valorCarta(carta);
+    puntosHTML[1].innerText = puntosComputadora;
+
+    const imgCarta = document.createElement("img");
+    imgCarta.src = `assets/cartas/${carta}.png`;
+    imgCarta.classList.add("carta");
+    divCartasComputadora.append(imgCarta);
+
+    if (puntosMinimos > 21) {
+      break;
+    }
+  } while (puntosComputadora < puntosMinimos && puntosMinimos <= 21);
+
+  setTimeout(() => {
+    if (puntosComputadora === puntosMinimos) {
+      alert("Nadie Gana :(^)");
+    } else if (puntosMinimos > 21) {
+      alert("Computadora gana");
+    } else if (puntosComputadora > 21) {
+      alert("Jugador Gana");
+    } else {
+      alert("Computadora Gana");
+    }
+  }, 10);
+};
 
 //Eventos
 btnPedir.addEventListener("click", () => {
@@ -81,8 +114,37 @@ btnPedir.addEventListener("click", () => {
   if (puntosJugador > 21) {
     console.warn("Lo siento mucho, perdiste");
     btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoComputadora(puntosJugador);
   } else if (puntosJugador === 21) {
     console.warn("21, genial!");
     btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoComputadora(puntosJugador);
   }
+});
+
+btnDetener.addEventListener("click", () => {
+  btnPedir.disabled = true;
+  btnDetener.disabled = true;
+
+  turnoComputadora(puntosJugador);
+});
+
+btnNuevo.addEventListener("click", () => {
+  console.clear();
+  deck = [];
+  deck = crearDeck();
+
+  puntosJugador = 0;
+  puntosComputadora = 0;
+
+  puntosHTML[0].innerText = 0;
+  puntosHTML[1].innerText = 0;
+
+  divCartasComputadora.innerHTML = "";
+  divCartasComputadora.innerHTML = "";
+
+  btnPedir.disabled = false;
+  btnDetener.disabled = false;
 });
